c98a39439428ae038cec1184365b82795e3e3d25
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-27 01:16:46 +0900

    ⚡️<Add>: “시작씬 버튼 메서드 수정”

--- Assets/Scripts/PersonalWorks/L_Script/StartScene/StartSceneManager.cs
+++ Assets/Scripts/PersonalWorks/L_Script/StartScene/StartSceneManager.cs
@@ -13,22 +13,12 @@ public class StartSceneManager : MonoBehaviour
     //새로하기버튼
     public void NewGame()
     {
-        PlayerPrefs.DeleteAll();//데이터를 삭제함
-        PlayerPrefs.Save();
-        SceneManager.LoadScene("TutorialScene");//튜툐리얼 씬 로드
+        SceneManager.LoadScene("GameScene");//게임 씬 로드
     }
     //이어하기버튼
-    public void ContinueGame()
+    public void Custom()
     {
-        if (PlayerPrefs.HasKey("SavedScene"))
-        {
-            string sceneToLoad = PlayerPrefs.GetString("SavedScene");
-            SceneManager.LoadScene(sceneToLoad);//저장된 씬을 로드
-        }
-        else
-        {
-            Debug.Log("저장데이터 없음");
-        }
+        SceneManager.LoadScene("CustomScene");//커스텀 씬 로드
     }
     //설정패널띄우기 버튼
     public void OpenSetting()
